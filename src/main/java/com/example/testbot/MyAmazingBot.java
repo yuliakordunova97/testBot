46f15f71dd79549b6b2a2577825d9cce2a500944
yuliakordunova97 <kordunovayulia@gmail.com>
2022-04-08 13:52:56 +0300

    done

---
+++ src/main/java/com/example/testbot/MyAmazingBot.java
@@ -0,0 +1,36 @@
+package com.example.testbot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // TODO
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return "yuliasTestBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return "5287733185:AAHU0QQi_ut8Lfn5imXDF8gFOOBfip5WOZM";
+    }
+}
---
+++ src/main/java/com/example/testbot/TestBotApplication.java
@@ -0,0 +1,24 @@
+package com.example.testbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class TestBotApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TestBotApplication.class, args);
+        System.out.println("Hello world");
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
